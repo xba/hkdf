b4e5c00b323091ec01536699e6ab44b48025d300
Karl McGuire <karl@karlmcguire.com>
2020-06-11 14:15:53 -0400

    cleanup

--- hkdf.go
+++ hkdf.go
@@ -8,31 +8,23 @@ import (
 	"math"
 )
 
-// Hash is a helper struct for passing to HKDF. HKDF needs to know the output
-// length (in bytes) of your hash.Hash function before running. If the 'salt'
-// parameter to HKDF is nil, a string of 0 bytes of length Len are used as the
-// salt.
-type Hash struct {
-	Hash func() hash.Hash
-	Len  int
-}
-
 // HKDF computes a PKM and OKM (where OKM is `l` bytes long) from the provided
-// parameters. If `salt` is nil, a string of `0x00` bytes of length `h.Len` are
-// used as the salt. If do not want a salt at all, just use `[]byte{}` as the
-// `salt` parameter.
-func HKDF(h Hash, salt, ikm, info []byte, l int) (prk, okm []byte) {
+// parameters. If `salt` is nil, a string of `0x00` bytes of length `h.Size()`
+// are used as the salt. If do not want a salt at all, just use `[]byte{}` as
+// the `salt` parameter.
+func HKDF(h func() hash.Hash, salt, ikm, info []byte, l int) (prk, okm []byte) {
 	if salt == nil {
-		salt = bytes.Repeat([]byte{0x00}, h.Len)
+		salt = bytes.Repeat([]byte{0x00}, h().Size())
 	}
-	f := hmac.New(h.Hash, salt)
+	f := hmac.New(h, salt)
 	f.Write(ikm)
 	prk = f.Sum(nil)
+	hl := len(prk)
 	okm = make([]byte, l, l)
-	f = hmac.New(h.Hash, prk)
-	for i := uint8(1); i <= uint8(math.Ceil(float64(l)/float64(h.Len))); i++ {
-		s := int(i-2) * h.Len
-		e := int(i-1) * h.Len
+	f = hmac.New(h, prk)
+	for i := uint8(1); i <= uint8(math.Ceil(float64(l)/float64(hl))); i++ {
+		s := int(i-2) * hl
+		e := int(i-1) * hl
 		if i != 1 {
 			f.Write(okm[s:e])
 		}

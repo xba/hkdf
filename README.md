78b706ebaa7d43eb904572b7915bffce8f55f2dc
Karl McGuire <karl@karlmcguire.com>
2020-06-11 14:00:46 -0400

    docs

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # hkdf
+
+[![GoDoc](https://img.shields.io/badge/api-reference-blue.svg)](https://godoc.org/github.com/xba/hkdf)
+[![Go Report Card](https://img.shields.io/badge/go%20report-A%2B-green.svg)](https://goreportcard.com/report/github.com/xba/hkdf)
+[![Coverage](https://img.shields.io/badge/coverage-100%25-ff69b4.svg)](https://gocover.io/xba/hkdf)
+
+[HMAC-based Extract-and-Expand Key Derivation Function
+(HKDF)](https://tools.ietf.org/html/rfc5869) for Go.
+
+## testing
+
+I'm using the test vectors described in the [official IETF HKDF
+paper](https://tools.ietf.org/html/rfc5869). You can execute the tests yourself
+by running:
+
+```
+$ go test
+```
--- hkdf.go
+++ hkdf.go
@@ -1,3 +1,4 @@
+// HKDF implements the HMAC-based Extract-and-Expand Key Derivation Function.
 package hkdf
 
 import (
@@ -7,19 +8,31 @@ import (
 	"math"
 )
 
-func HKDF(h func() hash.Hash, salt, ikm, info []byte, l int) (prk, okm []byte) {
+// Hash is a helper struct for passing to HKDF. HKDF needs to know the output
+// length (in bytes) of your hash.Hash function before running. If the 'salt'
+// parameter to HKDF is nil, a string of 0 bytes of length Len are used as the
+// salt.
+type Hash struct {
+	Hash func() hash.Hash
+	Len  int
+}
+
+// HKDF computes a PKM and OKM (where OKM is `l` bytes long) from the provided
+// parameters. If `salt` is nil, a string of `0x00` bytes of length `h.Len` are
+// used as the salt. If do not want a salt at all, just use `[]byte{}` as the
+// `salt` parameter.
+func HKDF(h Hash, salt, ikm, info []byte, l int) (prk, okm []byte) {
 	if salt == nil {
-		salt = bytes.Repeat([]byte{0x00}, 20)
+		salt = bytes.Repeat([]byte{0x00}, h.Len)
 	}
-	f := hmac.New(h, salt)
+	f := hmac.New(h.Hash, salt)
 	f.Write(ikm)
 	prk = f.Sum(nil)
-	hl := len(prk)
 	okm = make([]byte, l, l)
-	f = hmac.New(h, prk)
-	for i := uint8(1); i <= uint8(math.Ceil(float64(l)/float64(hl))); i++ {
-		s := int(i-2) * hl
-		e := int(i-1) * hl
+	f = hmac.New(h.Hash, prk)
+	for i := uint8(1); i <= uint8(math.Ceil(float64(l)/float64(h.Len))); i++ {
+		s := int(i-2) * h.Len
+		e := int(i-1) * h.Len
 		if i != 1 {
 			f.Write(okm[s:e])
 		}
